47a2cfbb3f3d309a4c84080fb88780242b451de5
cristinasanchezg <cristina.sancheg@alumnos.upm>
2015-05-12 20:45:01 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,25 +1,39 @@
 var models = require ('../models/models.js');
 
 // GET /quizes/:id
+exports.load =function (req, res, next, quizId){
+  models.Quiz.find(quizId).then(
+    function(quiz){
+      if(quiz){
+        req.quiz=quiz;
+        next();
+      }else{ next(new Error('No existe quizId=' + quizId));}
+    }
+    ).catch(function(error){next(error);});
+};
 
 exports.show = function (req, res) {
-   models.Quiz.find(req.params.quizId).then(function(quiz) {
-     res.render('quizes/show', {quiz: quiz});
-   })
+  // models.Quiz.find(req.params.quizId).then(function(quiz) {
+     res.render('quizes/show', {quiz: req.quiz});
+   //})
 };
 
 //GET /quizes/:id/answer
 
 exports.answer = function (req, res){
-   models.Quiz.find(req.params.quizId).then(function(quiz) {
-     if (req.query.respuesta === quiz.respuesta){
-           res.render ('quizes/answer', 
-                      { quiz: quiz, respuesta: 'Correcto'});
-     }else {
-           res.render('quizes/answer', 
-                      { quiz: quiz, respuesta: 'Incorrecto'});
+  // models.Quiz.find(req.params.quizId).then(function(quiz) {
+    var resultado= 'Incorrecto';
+     if (req.query.respuesta === req.quiz.respuesta){
+      resultado='Correcto';
      }
-   })
+     res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado}); 
+     //      res.render ('quizes/answer', 
+       //               { quiz: quiz, respuesta: 'Correcto'});
+    // }else {
+         //  res.render('quizes/answer', 
+           //           { quiz: quiz, respuesta: 'Incorrecto'});
+    // }
+  // })
 };
 
 //GET /quizes
