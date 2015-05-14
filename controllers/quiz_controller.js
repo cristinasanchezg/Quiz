3d7f67d70e54996b2c71770f585fb5c8d9288f5a
cristinasanchezg <cristina.sancheg@alumnos.upm>
2015-05-14 11:03:51 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -43,3 +43,22 @@ exports.index = function(req, res){
     res.render('quizes/index.ejs', { quizes: quizes});
   })
 };
+
+exports.new = function(req, res){
+  var quiz = models.Quiz.build( //crea objeto quiz
+    { pregunta: "Pregunta", respuesta: "Respuesta"}
+    );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+//POST /quizes/create
+
+exports.create = function(req, res){
+  var quiz = models.Quiz.build( req.body.quiz);
+
+  //guarda en DB los campos pregunta y respuesta del quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  }) //Redireccion HTTP (URL relativo) lista de preguntas
+};    
